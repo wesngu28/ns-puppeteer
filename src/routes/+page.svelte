3dd277f1cd8480a38dcd4d3839687d110e8935fb
wesngu28 <wesngu028@gmail.com>
2023-05-24 12:45:44 -0700

    feat: html content with basic styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header class="bg-gray-50 w-full text-center border-gray-200 border">
+    <h1 class="text-3xl my-8">NationStates Puppet Manager</h1>
+</header>
+
+<button class="mx-auto mt-4 h-10 mb-1 text-sm transition border-0 rounded appearance-none bg-blue-400 p-2 hover:bg-opacity-50">Load External CSV</button>
+
+<div class="flex flex-col">
+    <p class="mb-2">Enter Nations</p>
+    <textarea class='w-[600px] p-2 mb-1 text-sm bg-transparent border rounded appearance-none border-neutral-400 disabled:bg-neutral-100' placeholder="Nation" rows=15/>
+    <div>
+        <button class="mx-auto mt-4 h-10 mb-1 text-sm transition border-0 rounded appearance-none bg-blue-400 p-2 hover:bg-opacity-50">Start</button>
+        <button class="mx-auto mt-4 h-10 mb-1 text-sm transition border-0 rounded appearance-none bg-blue-400 p-2 hover:bg-opacity-50">Save to CSV</button>
+    </div>
+</div>
