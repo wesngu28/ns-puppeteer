7c53e6075bf271dab5a62689544d2a73abc91f9a
wesngu28 <wesngu028@gmail.com>
2023-05-24 12:47:37 -0700

    feat: bind input and save changes to local storage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+    import { onMount } from 'svelte'
+    let nationList = "Enter nations"
+    onMount(() => {
+        const store = localStorage.getItem("list");
+        if (store) nationList = store
+    })
+</script>
+
 <header class="bg-gray-50 w-full text-center border-gray-200 border">
     <h1 class="text-3xl my-8">NationStates Puppet Manager</h1>
 </header>
@@ -6,7 +15,7 @@
 
 <div class="flex flex-col">
     <p class="mb-2">Enter Nations</p>
-    <textarea class='w-[600px] p-2 mb-1 text-sm bg-transparent border rounded appearance-none border-neutral-400 disabled:bg-neutral-100' placeholder="Nation" rows=15/>
+    <textarea on:input={() => localStorage.setItem("list", nationList)} bind:value={nationList} class='w-[600px] p-2 mb-1 text-sm bg-transparent border rounded appearance-none border-neutral-400 disabled:bg-neutral-100' rows=15/>
     <div>
         <button class="mx-auto mt-4 h-10 mb-1 text-sm transition border-0 rounded appearance-none bg-blue-400 p-2 hover:bg-opacity-50">Start</button>
         <button class="mx-auto mt-4 h-10 mb-1 text-sm transition border-0 rounded appearance-none bg-blue-400 p-2 hover:bg-opacity-50">Save to CSV</button>
