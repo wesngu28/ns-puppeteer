b7121beb6a1c3444f0b3c461cecca3659f156d33
wesngu28 <wesngu028@gmail.com>
2023-05-25 18:40:46 -0700

    feat: iframe for restore/login, credit to auralia

---
+++ src/lib/helpers/restoreRequest.ts
@@ -0,0 +1,33 @@
+import { sleep } from "./sleep";
+
+export async function restoreRequest(userAgent: string, username: string, password: string) {
+    let resolved = false;
+
+    const iframe = document.getElementById("iframe")! as HTMLIFrameElement;
+    iframe.removeEventListener("load", loadHandler);
+    iframe.addEventListener("load", loadHandler);
+
+    async function loadHandler(this: any) {
+        iframe.removeEventListener("load", loadHandler);
+        const iframeContents = iframe.contentWindow?.document! as any;
+
+        if (iframeContents) {
+            iframeContents.getElementById("restoreUserAgent").value = userAgent;
+            iframeContents.getElementById("restoreLoggingIn").value = "1";
+            iframeContents.getElementById("restoreNation").value = username;
+            iframeContents.getElementById("restoreRestoreNation").value = `Restore ${username}`;
+            iframeContents.getElementById("restoreRestorePassword").value = password;
+            iframeContents.getElementById("restoreSubmit").click();
+
+            await sleep(6000)
+            return true
+        }
+    }
+
+    iframe.src = "iframe.html";
+
+    if (!resolved) {
+        await sleep(15000)
+        return false
+    }
+}
