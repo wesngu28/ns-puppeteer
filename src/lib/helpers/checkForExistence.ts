88861da0e94b678166dad8479f6fa2528a8ea7a3
wesngu28 <wesley@wesngu28.com>
2023-05-25 20:49:32 -0700

    refactor: pinging should suffice for nation status

--- src/lib/helpers/checkForExistence.ts
+++ src/lib/helpers/checkForExistence.ts
@@ -1,21 +1,13 @@
-import type { XMLParser } from "fast-xml-parser";
-
-export async function checkForExistence(userAgent: string, parser: XMLParser, username: string, password: string) {
-    const res = await fetch(`https://www.nationstates.net/cgi-bin/api.cgi?nation=${username}`)
-    const existence = res.status
-    if (existence === 200) {
-        const rawxml = await res.text()
-        let xml = parser.parse(rawxml);
-        const response = await fetch(`https://www.nationstates.net/cgi-bin/api.cgi?nation=${username}&q=ping`, { 
+export async function checkForExistence(userAgent: string, username: string, password: string) {
+    const res = await fetch(`https://www.nationstates.net/cgi-bin/api.cgi?nation=${username}&q=ping`,
+        {
             headers: {
                 'X-Password': password,
                 'User-Agent': userAgent
             }
-            });
-        if (response.status === 200) {
-            return `Successfully logged into ${xml.NATION.FULLNAME}`
-        }
-        return "Not found"
-    }
+        })
+    const existence = res.status
+    if (existence === 404) return "Not found"
+    if (existence === 200) return `Successfully logged into ${username}`
     return "Not found"
 }
